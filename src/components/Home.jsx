1c17aecd2f34c2b9a8bd489c647565d6157bb909
Kauany-Santos <165852032+Kauany-Santos@users.noreply.github.com>
2025-04-01 20:44:39 -0300

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
-import Sidebar from "./Sidebar";
-import Header from "./Header";
-import Carousel from "./Carousel";
-import Recommended from "./Recommended";
+import Sidebar from "src/components/Sidebar.jsx";
+import Header from "src/components/Header.jsx";
+import Carousel from "src/components/Carousel.jsx";
+import Recommended from "src/components/Recommended.jsx";
 
 export default function Home() {
   return (
